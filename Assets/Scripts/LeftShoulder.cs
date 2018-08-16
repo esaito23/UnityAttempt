ef6f065f50ae46312f9ad5d1cd85e338cb0c5b86
esaito23 <emily@fablab.com>
2018-08-15 20:39:36 -0700

    Unity files

---
+++ Assets/Scripts/LeftShoulder.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+using System.IO;
+
+public class LeftShoulder : MonoBehaviour
+{	
+
+
+
+		string line;
+
+
+		static string filename = "Assets/Resources/LeftArm.txt";
+
+		StreamReader theReader = new StreamReader(filename);
+
+
+	// Use this for initialization
+	void Start()
+	{
+
+		line = theReader.ReadLine ();
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (line != null) {
+			string[] entries = line.Split (',');
+
+			Quaternion rot = Quaternion.Euler (new Vector3 (float.Parse (entries [3]) * 100, float.Parse (entries [4]) * 100, float.Parse (entries [5]) * 100));
+			transform.rotation = Quaternion.Slerp (transform.rotation, rot, Time.deltaTime * 2.0f);
+
+
+			//transform.Translate (float.Parse(entries [0]) / 100, float.Parse(entries [1]) / 100, float.Parse(entries [2]) / 100);
+
+
+			line = theReader.ReadLine ();
+		}
+	}
+
+}
---
+++ Assets/Scripts/RightThigh.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Text;
+using System.IO;
+
+public class RightThigh : MonoBehaviour
+{	
+
+
+
+	string line;
+
+
+	static string filename = "Assets/Resources/RightLeg.txt";
+
+	StreamReader theReader = new StreamReader(filename);
+
+
+	// Use this for initialization
+	void Start()
+	{
+
+		line = theReader.ReadLine ();
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (line != null) {
+			string[] entries = line.Split (',');
+
+			Quaternion rot = Quaternion.Euler (new Vector3 (float.Parse (entries [3]), float.Parse (entries [4]), float.Parse (entries [5]) - 45));
+			transform.rotation = Quaternion.Slerp (transform.rotation, rot, Time.deltaTime * 2.0f);
+
+
+			//transform.Translate (float.Parse(entries [0]) / 100, float.Parse(entries [1]) / 100, float.Parse(entries [2]) / 100);
+
+
+			line = theReader.ReadLine ();
+		}
+	}
+
+}
